e68f1763d4b2a2e75e3540043ee32f6cec484a63
JLMaikl <jlmaikl@mail.ru>
2022-07-07 12:52:58 +0300

    Finished project

---
+++ components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SiBurgerking } from 'react-icons/si';
+
+const Footer = () => {
+  return (
+    <footer>
+        Copyright 2022 - For Burgers
+    </footer>
+  );
+};
+
+export default Footer;
